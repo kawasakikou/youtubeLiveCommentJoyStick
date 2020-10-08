af0757eae61f525a1788322850975051547917d2
kou <forte0801@gmail.com>
2020-10-08 09:34:59 +0900

    [wip]Add raspberry pi gpio code

--- gameController/gpio.ts
+++ gameController/gpio.ts
@@ -1,8 +1,60 @@
-import { init } from 'raspi';
-import { DigitalInput, DigitalOutput } from 'raspi-gpio';
+import fs from 'fs';
 
-init(() => {
-  const input = new DigitalInput('P1-3');
-  const output = new DigitalOutput('P1-5');
-  output.write(input.read());
-});
+
+export class GPIO {
+  registeredPins: number[];
+
+  constructor() {
+    this.registeredPins = [];
+  }
+
+  setup = (gpioNumber: number, pinType: PinType, initial: Initial) => {
+    fs.writeFileSync('/sys/class/gpio/export', gpioNumber);
+
+    const pinTypeValue: string = pinType == 'IN' ? 'in' : 'out';
+    fs.writeFileSync(`/sys/class/gpio/gpio${gpioNumber}/direction`, pinTypeValue);
+
+    const initialValue: number = initial == 'LOW' ? 0 : 1;
+    fs.writeFileSync(`/sys/class/gpio/gpio${gpioNumber}/value`, initialValue);
+    this.registeredPins.push(gpioNumber)
+  };
+
+  cleanUp() {
+    for (const pinNumber of this.registeredPins) {
+      fs.writeFileSync('/sys/class/gpio/unexport', pinNumber);
+    }
+  }
+
+  async on(pinNumber: number) {
+    fs.writeFileSync(`/sys/class/gpio/gpio${pinNumber}/value`, 0);
+    await sleep(1000);
+    fs.writeFileSync(`/sys/class/gpio/gpio${pinNumber}/value`, 1);
+  }
+
+  async keySelect(inputKey: string) {
+    if (inputKey == 'a') {
+      await this.on(17)
+    } else if (inputKey == 'b') {
+      await this.on(27)
+    } else if (inputKey == 'home') {
+      await this.on(23)
+    } else if (inputKey == 'game') {
+      await this.on(24)
+    } else if (inputKey == 'u') {
+      await this.on(16)
+    } else if (inputKey == 'd') {
+      await this.on(20)
+    } else if (inputKey == 'r') {
+      await this.on(21)
+    } else if (inputKey == 'l') {
+      await this.on(26)
+    } else {
+      ;
+    }
+  }
+}
+
+type PinType = 'IN' | 'OUT'
+type Initial = 'HIGH' | 'LOW'
+
+const sleep = (msec: number) => new Promise(resolve => setTimeout(resolve, msec));
--- index.ts
+++ index.ts
@@ -1,5 +1,33 @@
+import { GPIO } from './gameController/gpio';
+import fs from 'fs';
+
 async function main() {
-  console.log('hello');
+  const gpio = new GPIO();
+  gpio.setup(17, 'OUT', 'HIGH');
+  gpio.setup(27, 'OUT', 'HIGH');
+  gpio.setup(23, 'OUT', 'HIGH');
+  gpio.setup(24, 'OUT', 'HIGH');
+
+  gpio.setup(16, 'OUT', 'HIGH');
+  gpio.setup(20, 'OUT', 'HIGH');
+  gpio.setup(21, 'OUT', 'HIGH');
+  gpio.setup(26, 'OUT', 'HIGH');
+
+  process.on('SIGINT', function() {
+    gpio.cleanUp();
+    process.exit();
+  });
+
+  try {
+    setInterval(() => {
+      console.log('>');
+      const key: string = 'u';
+      console.log(key);
+      gpio.keySelect(key)
+    }, 1500);
+  } catch (e) {
+    gpio.cleanUp();
+  }
 }
 
 main().catch((err) => {
